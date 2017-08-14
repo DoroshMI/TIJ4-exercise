cc1cf77571b1fdc8c4db500458174d556f1a1629
Maryan Dorosh <dorosh.maryan@gmail.com>
2017-08-14 18:51:58 +0300

    Exercise5

--- Chapter14TypeInformation/src/main/java/com/dorosh/lab/Ch14Ex05.java
+++ Chapter14TypeInformation/src/main/java/com/dorosh/lab/Ch14Ex05.java
@@ -15,22 +15,9 @@ package com.dorosh.lab;
 import java.util.Arrays;
 import java.util.List;
 
-public class Ch14Ex04 {
-
-	public static void rotate(Shape s) {
-		if (!(s instanceof Circle))
-			System.out.println(s + " rotate");
-	}
-
+public class Ch14Ex05 {
 	public static void main(String[] args) {
-		List<Shape> shapeList = Arrays.asList(new Circle(), new Square(), new Triangle(), new Rhomboid());
-		for (Shape shape : shapeList)
-			shape.draw();
-
-		System.out.println();
-		for (Shape shape : shapeList)
-			rotate(shape);
-
+		
 	}
 
 }
